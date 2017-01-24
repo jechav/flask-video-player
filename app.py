ca212c94a0473b2eaf6e0e3decbf814137cc1dbf
jochechavez <joche.echavezm@gmail.com>
2017-01-24 02:18:05 -0500

    initial configuraion

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html');
+
+@app.route('/v/')
+def videoplayer():
+    if not request.args.get('url'): return redirect('/')
+    return render_template('videoplayer.html', url=request.args.get('url'));
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
